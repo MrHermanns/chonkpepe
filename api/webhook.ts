29e809204cfea79951d560f585608de13827572e
MrHermanns <jeremy@jeremyhermanns.org>
2025-07-14 14:38:40 -0700

    Update webhook.ts

--- api/webhook.ts
+++ api/webhook.ts
@@ -3,7 +3,17 @@ import bot from '../bot'
 
 export default async function (req: VercelRequest, res: VercelResponse) {
   const { body } = req
+  if (!body.message) {
+    res.status(204).send('')
+    return
+  }
   const { chat: { id }, text } = body.message
-  await bot.sendMessage(id, text)
+
+  if (text === '/start') {
+    await bot.sendMessage(id, '🐸 Welcome to ChonkPepe! Tap the game button below to start playing.');
+    // (Later, you can add a button here)
+  } else {
+    await bot.sendMessage(id, "Send /start to begin playing ChonkPepe!");
+  }
   res.status(204).send('')
 }
